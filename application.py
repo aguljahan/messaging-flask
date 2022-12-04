b9d139b1e494e785d747a2c492155db0253b84b8
Guljahan Annagurbanova <aguljahan@sabanciuniv.edu>
2022-12-04 09:39:39 +0300

    sockets

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
 application = Flask(__name__)
+application.config['SECRET_KEY'] = 'somesecret'
+socketio = SocketIO(application)
 
 @application.route('/')
-def hello_world():
-    return 'Hello Jerenochka'
+def home():
+    return render_template('index.html')
+
+
+@socketio.on('message')
+def handle_message(message):
+    print('Message received: ' + message)
+    if message != "User Connected":
+        send(message, broadcast=True)
+
+
+if __name__ == '__main__':
+    socketio.run(application, debug=True)
